1174fcc4237d0554c38319607f661f3cd6224642
varelapj <jesusjorgevarelaperez@hotmail.com>
2023-09-28 21:02:59 +0200

    prueba Jorge

---
+++ JS_HTML_CSS/JAVASCRIPT/EJERCICIOS/BASIC_ALGORITHMS/app.js
@@ -0,0 +1,55 @@
+console.log("ESTA ES LA ITERACIÓN 1");
+let myFavoriteHero ;
+myFavoriteHero = "Hulk"
+
+console.log (myFavoriteHero)
+
+const X = 50
+const h = 5
+const y =10
+let z = h + y
+console.log (z)
+
+//!------------------INTERACTION2
+console.log("ESTA ES LA ITERACIÓN 2");
+const character = {name: 'Jack Sparrow', age: 10}
+character.age = 25
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log ("Soy "+ firstName)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log (toy1.price + toy2.price)
+
+//! Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+//! y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+//! basePrice más el valor de la variable globalBasePrice.
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice
+console.log (car1);
+console.log (car2)
+
+
+//!------------------INTERACTION3
+console.log("ESTA ES LA ITERACIÓN 3");
+
+console.log (10*5);
+console.log (10*2);
+console.log (15*9);
+
+let p = 10;
+let j = 5;
+console.log (p+j);
+console.log (p*j)
+
+
+
---
+++ JS_HTML_CSS/JAVASCRIPT/EJERCICIOS/OP_LOGICOS_MINI_RETO/app.js
@@ -0,0 +1,27 @@
+
+//!------------------MINI-RETO
+console.log("ESTE ES EL MINI-RETO DE OPERADORES LÓGICOS");
+
+let planet;
+planet = 'Tierra';
+
+let isInnerPlanet;
+isInnerPlanet = false;
+
+let hasAtmosphere;
+hasAtmosphere = false;
+
+let isHabitable = isInnerPlanet && hasAtmosphere; //! || or
+console.log(isHabitable)
+
+//!------------------EXTRA
+
+if(isInnerPlanet && hasAtmosphere){
+    console.log (`La ${planet} es habitable`) //! template string
+} else {
+    console.log (`La ${planet} no es habitable`)
+}
+console.log(isHabitable)
+
+//!console.log ("La " + planet + " es habitable") ;
+//!console.log ("La " && planet && " es habitable") 
