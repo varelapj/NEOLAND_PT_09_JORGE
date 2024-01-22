662a2af639ddb327bb7fc1f3f242360549c0f350
varelapj <jesusjorgevarelaperez@hotmail.com>
2024-01-22 18:19:54 +0100

    240123

---
+++ NODE/PRACTICA/utils/db.js
@@ -0,0 +1,39 @@
+//!------------ REQUERIMOS DOTENV --------------
+
+const dotenv = require("dotenv");
+// usamos dotenv
+dotenv.config();
+//!------------ REQUERIMOS MONGOOSE --------------
+
+const mongoose = require("mongoose");
+
+
+//!---------- NOS TRAEMOS MONGO_URI DE .ENV
+
+const MONGO_URI = process.env.MONGO_URI;
+
+//!----------- CREAMOS FUNCIÓN QUE CONECTA CON NUESTRA BASE DE DATOS
+
+const connect = async () => {
+  // --------- try : intenta hacer algo ..... y sino lo capturas catch
+  try {
+    // conectamos con nuestra base de datos
+    const db = await mongoose.connect(MONGO_URI, {
+      // parsea la url de MONGO
+      useNewUrlParser: true,
+      // convertir los caracteres especiales
+      useUnifiedTopology: true,
+    });
+
+    // Hacemos destructuring de nombre y host de nuestra base de datos, entrando en la constante db, función .connection
+    const { name, host } = db.connection;
+
+    console.log(
+      `Conectados a la DB 👌🏾 con el nombre: ${name} en el host: ${host} `
+    );
+  } catch (error) {
+    console.log("Hay en un error en la conexión ❌, ", error);
+  }
+};
+
+module.exports = { connect };
